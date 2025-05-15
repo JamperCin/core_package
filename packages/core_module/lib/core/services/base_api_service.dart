990058d629cce71f0de94ef49f14c1e295794801
jackbuild <jackghsp@gmail.com>
2025-05-15 14:52:37 +0000

    Fixed base api service

--- packages/core_module/lib/core/services/base_api_service.dart
+++ packages/core_module/lib/core/services/base_api_service.dart
@@ -15,28 +15,31 @@ class BaseApiService {
     bool multiPartHeader = false,
     param,
     Encoding? encoding,
+    Map<String, String>? headers,
   }) async {
     debugPrint(
-        "\nPOST REQUEST: $uri\nHEADERS: ${multiPartHeader ? _getMultiPartTokenHeaders() : (tokenHeader ? _getTokenHeaders() : _getHeaders())} \nBODY : $param");
+        "\nPOST REQUEST: $uri\nHEADERS: ${headers ?? (multiPartHeader ? _getMultiPartTokenHeaders() : (tokenHeader ? _getTokenHeaders() : _getHeaders()))} \nBODY : $param");
     return http.post(
       uri,
       body: param,
       encoding: encoding,
-      headers: multiPartHeader
-          ? _getMultiPartTokenHeaders()
-          : (tokenHeader ? _getTokenHeaders() : _getHeaders()),
+      headers: headers ??
+          (multiPartHeader
+              ? _getMultiPartTokenHeaders()
+              : (tokenHeader ? _getTokenHeaders() : _getHeaders())),
     );
   }
 
   Future<http.Response> _get(
     uri, {
     bool tokenHeader = true,
+    Map<String, String>? headers,
   }) async {
     debugPrint(
-        "\nGET REQUEST: $uri\nHEADERS: ${tokenHeader ? _getTokenHeaders() : _getHeaders()}");
+        "\nGET REQUEST: $uri\nHEADERS: ${headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders())}");
     return http.get(
       uri,
-      headers: tokenHeader ? _getTokenHeaders() : _getHeaders(),
+      headers: headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders()),
     );
   }
 
@@ -45,14 +48,15 @@ class BaseApiService {
     bool tokenHeader = true,
     body,
     Encoding? encoding,
+    Map<String, String>? headers,
   }) async {
     debugPrint(
-        "$url, requestType: PUT, headers: ${tokenHeader ? _getTokenHeaders() : _getHeaders()}, params: $body");
+        "$url, requestType: PUT, headers: ${headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders())}, params: $body");
     return http.put(
       url,
       body: body,
       encoding: encoding,
-      headers: tokenHeader ? _getTokenHeaders() : _getHeaders(),
+      headers: headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders()),
     );
   }
 
@@ -61,14 +65,15 @@ class BaseApiService {
     bool tokenHeader = true,
     body,
     Encoding? encoding,
+    Map<String, String>? headers,
   }) async {
     debugPrint(
-        "$url, requestType: DELETE, headers: ${tokenHeader ? _getTokenHeaders() : _getHeaders()}, params: $body");
+        "$url, requestType: DELETE, headers: ${headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders())}, params: $body");
     return http.delete(
       url,
       body: body,
       encoding: encoding,
-      headers: tokenHeader ? _getTokenHeaders() : _getHeaders(),
+      headers: headers ?? (tokenHeader ? _getTokenHeaders() : _getHeaders()),
     );
   }
 
@@ -76,9 +81,10 @@ class BaseApiService {
     required File file,
     required Uri uri,
     MediaType? mediaType,
+    Map<String, String>? headers,
   }) async {
     var request = http.MultipartRequest("POST", uri);
-    request.headers.addAll(_getMultiPartTokenHeaders());
+    request.headers.addAll(headers ?? _getMultiPartTokenHeaders());
     String fileName = "";
     if (mediaType == null || mediaType.type == "image") {
       fileName = "prime_customer${DateTime.now().millisecond}_img.jpg";
@@ -110,7 +116,6 @@ class BaseApiService {
       queryParameters: body,
     );
   }
-
 
   Map<String, String> _getHeaders() {
     Map<String, String> headers = {};
@@ -161,7 +166,8 @@ class BaseApiService {
       }
 
       if (print) {
-        debugPrint("Response : $json\n");
+        debugPrint(
+            "Response : $json\n --> Status Code : ${response.statusCode}");
       }
 
       bool success =
@@ -172,7 +178,11 @@ class BaseApiService {
           return parser(json);
         }
 
-        return json is Map ? json as Map<String, dynamic> : json;
+        return json is Map
+            ? parser != null
+                ? parser(json as Map<String, dynamic>)
+                : json as Map<String, dynamic>
+            : json;
       } else {
         String error = getObject('error', json) ??
             getObject('error_msg', json) ??
@@ -186,7 +196,8 @@ class BaseApiService {
     } catch (e) {
       debugPrint("Error $e");
       if (showToast) {
-        snackBarSnippet.snackBarError("An error occurred. Please try again later");
+        snackBarSnippet
+            .snackBarError("An error occurred. Please try again later");
       }
       return null;
     }
@@ -213,12 +224,14 @@ class BaseApiService {
     required String key,
     String? objectKey,
     bool showToast = false,
+    bool print = true,
     required T Function(Map<String, dynamic>) fromJson,
     Map<String, dynamic>? param,
   }) async {
     final results = await getRequest<List<T>>(
             api: api,
             param: param,
+            print: print,
             showToast: showToast,
             parser: (json) {
               final listMap = json[objectKey ?? 'data'][key] as List;
@@ -232,15 +245,18 @@ class BaseApiService {
     required String api,
     Map<String, dynamic>? param,
     bool tokenHeader = true,
+    bool print = true,
     bool showToast = false,
     String? host,
+    Map<String, String>? headers,
     T Function(dynamic)? parser,
   }) async {
     final uri = await _buildUrl(api, body: param, host: host);
-    var response = await _get(uri, tokenHeader: tokenHeader);
+    var response = await _get(uri, tokenHeader: tokenHeader, headers: headers);
     return _serialiseResponse<T>(
       response: response,
       parser: parser,
+      print: print,
       showToast: showToast,
     );
   }
@@ -249,19 +265,73 @@ class BaseApiService {
     required String api,
     Map<String, dynamic>? param,
     bool tokenHeader = true,
+    bool print = true,
     bool showToast = false,
     String? host,
+    Map<String, String>? headers,
     T Function(dynamic)? parser,
   }) async {
     final uri = await _buildUrl(api, host: host);
     var response = await _post(
       uri,
       tokenHeader: tokenHeader,
+      headers: headers,
       param: param != null ? jsonEncode(param) : param,
     );
     return _serialiseResponse<T>(
       response: response,
       parser: parser,
+      print: print,
+      showToast: showToast,
+    );
+  }
+
+  Future<T?> putRequest<T>({
+    required String api,
+    Map<String, dynamic>? param,
+    bool tokenHeader = true,
+    bool print = true,
+    bool showToast = false,
+    String? host,
+    Map<String, String>? headers,
+    T Function(dynamic)? parser,
+  }) async {
+    final uri = await _buildUrl(api, host: host);
+    var response = await _put(
+      uri,
+      tokenHeader: tokenHeader,
+      headers: headers,
+      body: param != null ? jsonEncode(param) : param,
+    );
+    return _serialiseResponse<T>(
+      response: response,
+      parser: parser,
+      print: print,
+      showToast: showToast,
+    );
+  }
+
+  Future<T?> deleteRequest<T>({
+    required String api,
+    Map<String, dynamic>? param,
+    bool tokenHeader = true,
+    bool showToast = false,
+    bool print = true,
+    String? host,
+    Map<String, String>? headers,
+    T Function(dynamic)? parser,
+  }) async {
+    final uri = await _buildUrl(api, host: host);
+    var response = await _delete(
+      uri,
+      tokenHeader: tokenHeader,
+      headers: headers,
+      body: param != null ? jsonEncode(param) : param,
+    );
+    return _serialiseResponse<T>(
+      response: response,
+      parser: parser,
+      print: print,
       showToast: showToast,
     );
   }
@@ -270,15 +340,18 @@ class BaseApiService {
     required String api,
     required File file,
     bool showToast = false,
+    bool print = true,
     String? host,
+    Map<String, String>? headers,
     T Function(dynamic)? parser,
   }) async {
     final uri = await _buildUrl(api, host: host);
-    var response = await _postMultipart(file: file, uri: uri);
+    var response = await _postMultipart(file: file, uri: uri, headers: headers);
     final respStr = await response.stream.bytesToString();
     return _serialiseResponse<T>(
       response: respStr,
       parser: parser,
+      print: print,
       showToast: showToast,
     );
   }
