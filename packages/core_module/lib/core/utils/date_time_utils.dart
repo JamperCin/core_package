d1c9891932917db89f640c5d5716717777fb6f2b
jackbuild <jackghsp@gmail.com>
2025-06-30 22:43:23 +0000

    Update DateTime utils

--- packages/core_module/lib/core/utils/date_time_utils.dart
+++ packages/core_module/lib/core/utils/date_time_utils.dart
@@ -9,8 +9,21 @@ class DateTimeUtils {
     return _instance ??= DateTimeUtils._internal();
   }
 
-  String formatDate(String dateStr, {String format = 'EEE, MMM d yyyy, hh:mm a'}) {
-    DateTime parsedDate = DateTime.parse(dateStr);
-    return DateFormat(format).format(parsedDate);
+  String formatDate(String dateStr,
+      {String format = 'EEE, MMM d yyyy, hh:mm a'}) {
+    try {
+      DateTime parsedDate = DateTime.parse(dateStr);
+      return DateFormat(format).format(parsedDate);
+    } catch (e) {
+      return dateStr;
+    }
+  }
+
+  String formatDateTime(DateTime date, {String format = 'EEE, MMM d yyyy, hh:mm a'}) {
+    return DateFormat(format).format(date);
+  }
+
+  String getCurrentDate({String format = 'yyyy-MM-dd'}) {
+    return DateFormat(format).format(DateTime.now());
   }
 }
