b97d14338e3c9573ad3937eb9211319b0209e461
jackbuild <jackghsp@gmail.com>
2025-08-11 17:08:00 +0000

    Updated fileUtils

--- packages/core_module/lib/core/utils/file_utils.dart
+++ packages/core_module/lib/core/utils/file_utils.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:core_module/core/utils/map_utils.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class FileUtils {
   static FileUtils? _fileUtils;
@@ -51,8 +55,6 @@ class FileUtils {
                 : [];
           }
           return [];
-          // final listMap = ((objectKey != null ? data[objectKey] : data ) as Map<String, dynamic>)[key] as List;
-          // return [...listMap.map((e) => parser(e))];
         });
     return results;
   }
@@ -88,13 +90,14 @@ class FileUtils {
     return results;
   }
 
+
+  // 0x41 is Letter A
+  // 0x1F1E6 is Regional Indicator Symbol Letter A
+  // Example :
+  // firstLetter U => 20 + 0x1F1E6
+  // secondLetter S => 18 + 0x1F1E6
+  // See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
   static Future<String> countryCodeToEmoji(String countryCode) async{
-    // 0x41 is Letter A
-    // 0x1F1E6 is Regional Indicator Symbol Letter A
-    // Example :
-    // firstLetter U => 20 + 0x1F1E6
-    // secondLetter S => 18 + 0x1F1E6
-    // See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
     try {
       final int firstLetter = countryCode.codeUnitAt(0) - 0x41 + 0x1F1E6;
       final int secondLetter = countryCode.codeUnitAt(1) - 0x41 + 0x1F1E6;
@@ -104,4 +107,54 @@ class FileUtils {
       return countryCode;
     }
   }
+
+
+  ///1. Find the correct local path
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  ///2. Create a reference to the file location
+  Future<File> _localFile(String fileName) async {
+    final path = await _localPath;
+    return File('$path/$fileName');
+  }
+
+  ///3. Write data to the file
+  Future<File> writeData(String data, String fileName) async {
+    final file = await _localFile(fileName);
+
+    // Write the file
+    return file.writeAsString(data);
+  }
+
+  ///4. Read data from the file
+  Future<String> readData(String fileName) async {
+    try {
+      final file = await _localFile(fileName);
+
+      // Read the file
+      final contents = await file.readAsString();
+
+      return contents;
+    } catch (e) {
+      // If encountering an error, return 0
+      return "";
+    }
+  }
+
+   Future<String> downloadFile({String url = '', String? fileName}) async {
+    if (url.isEmpty) {
+      return "";
+    }
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = "${directory.path}/${fileName ?? url.split('/').last}";
+    final response = await http.get(Uri.parse(url));
+    final file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
+
 }
