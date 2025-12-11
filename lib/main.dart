6e19aca2ba29e713076a68662c7edb78b2751252
jackbuild <jackghsp@gmail.com>
2025-12-11 22:31:09 +0000

    fix errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
-import 'package:core_module/core/app/app_dimens.dart';
-import 'package:core_module/core/app/app_style.dart';
-import 'package:core_module/core/app/app_theme.dart';
-import 'package:core_module/core/def/global_def.dart';
-import 'package:core_module/core/extensions/int_extension.dart';
-import 'package:core_module/core/model/local/dictionary_model.dart';
-import 'package:core_module/core/res/assets_path.dart';
 import 'package:core_module/core_module.dart';
-import 'package:core_module/core_ui/snippets/places_search/places_picker_widget.dart';
-import 'package:core_module/core_ui/widgets/button_switch_widget.dart';
-import 'package:core_module/core_ui/widgets/button_widget.dart';
-import 'package:core_module/core_ui/widgets/shimmer_widget.dart';
 import 'package:core_module_package/login_screen.dart';
 import 'package:core_module_package/res/app_theme.dart';
 import 'package:core_module_package/res/res_path.dart';
@@ -40,11 +29,11 @@ class MyApp extends StatelessWidget {
     appDimen = AppDimens(context, constantMultiplier: 1.75);
 
     return Obx(
-      ()=> GetMaterialApp(
+          ()=> GetMaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: isDarkMode.value ? darkMode : lightMode,
-       // darkTheme: darkMode,
+        // darkTheme: darkMode,
         initialRoute: '/',
         getPages: [
           GetPage(name: '/LoginScreen', page: () => LoginScreen()),
@@ -67,12 +56,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final RxList<DictionaryModel> _items = <DictionaryModel>[
-    DictionaryModel(key: "key1", value: "value1"),
-    DictionaryModel(key: "key2", value: "value2", selected: true),
-    DictionaryModel(key: "key3", value: "value3"),
-    DictionaryModel(key: "key4", value: "value4"),
-  ].obs;
 
   void _incrementCounter() {
     // setState(() {
@@ -129,38 +112,13 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Padding(
-          padding: EdgeInsets.all(20.dp()),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "counter counts here",
-                style: labelSmallTextLightModeStyle,
-              ),
-              ShimmerWidget(
-                child: Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ),
-              ButtonWidget.withOutLine(
-                text: 'Click Here ',
-                asset: icApple,
-                onTap: () {
-                  navUtils.fireTargetHome();
-                },
-              ),
-              SizedBox(height: 20.dp()),
-              ButtonSwitchWidget(
-                items: _items,
-                onTap: (model) {
-                  print("model --> ${model.value}");
-                },
-              ),
+      body: Column(
+        children: [
+          TabBarWidget(
+            tabs: const [
+              Tab(text: "Available Books"),
+
+              Tab(text: "Purchased Books"),
             ],
             onTap: (index) {
               isLoadingMore.value = index == 0;
@@ -168,42 +126,42 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Expanded(
             child: Obx(
-              () => isLoadingMore.value
+                  () => isLoadingMore.value
                   ? ListViewWidget<BottomBarModel>(
-                      list: bottomBarItems,
-                      onLoadMore: _loadMore,
-                      listItemWidget: (item) {
-                        return Column(
-                          children: [
-                            Text(item.text ?? ''),
-                            Gap(20.dp()),
-                            AssetImageWidget(
-                              asset: item.asset ?? '',
-                              height: 40.dp(),
-                              width: 40.dp(),
-                            )
-                          ],
-                        );
-                      },
-                    )
+                list: bottomBarItems,
+                onLoadMore: _loadMore,
+                listItemWidget: (item) {
+                  return Column(
+                    children: [
+                      Text(item.text ?? ''),
+                      Gap(20.dp()),
+                      AssetImageWidget(
+                        asset: item.asset ?? '',
+                        height: 40.dp(),
+                        width: 40.dp(),
+                      )
+                    ],
+                  );
+                },
+              )
                   : ListViewWidget<BottomBarModel>.withGridView(
-                      list: bottomBarItems,
-                      onLoadMore: _loadMore,
-                      listItemWidget: (item) {
-                        return Column(
-                          children: [
-                            Text(item.text ?? ''),
-                            Gap(20.dp()),
-                            AssetImageWidget(
-                              asset: item.asset ?? '',
-                              height: 40.dp(),
-                              width: 40.dp(),
-                            )
-                          ],
-                        );
-                      },
+                list: bottomBarItems,
+                onLoadMore: _loadMore,
+                listItemWidget: (item) {
+                  return Column(
+                    children: [
+                      Text(item.text ?? ''),
+                      Gap(20.dp()),
+                      AssetImageWidget(
+                        asset: item.asset ?? '',
+                        height: 40.dp(),
+                        width: 40.dp(),
+                      )
+                    ],
+                  );
+                },
 
-                    ),
+              ),
             ),
           ),
         ],
